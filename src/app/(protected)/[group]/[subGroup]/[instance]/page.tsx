c7758373b4809df4689e8877351197b63cf1815f
Petros <93052465+pkitazos@users.noreply.github.com>
2025-09-11 17:19:30 +0300

    Refactored instance home page to be a little more sane - single component which renders multiple sections based on roles - each role has a separate home component which matches on the current instance stage for displaying different tasks - removed parallel routes cause they're dumb

--- src/app/(protected)/[group]/[subGroup]/[instance]/page.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/page.tsx
@@ -1,9 +1,21 @@
 import { app, metadataTitle } from "@/config/meta";
 
+import { Role } from "@/db/types";
+
 import { Heading } from "@/components/heading";
+import { JoinInstance } from "@/components/join-instance";
+import { PanelWrapper } from "@/components/panel-wrapper";
+import { Separator } from "@/components/ui/separator";
 
 import { api } from "@/lib/trpc/server";
 import { type InstanceParams } from "@/lib/validations/params";
+
+import {
+  AdminHome,
+  ReaderHome,
+  StudentHome,
+  SupervisorHome,
+} from "./_components/instance-home";
 
 export async function generateMetadata({ params }: { params: InstanceParams }) {
   const { displayName } = await api.institution.instance.get({ params });
@@ -14,5 +26,31 @@ export async function generateMetadata({ params }: { params: InstanceParams }) {
 export default async function Page({ params }: { params: InstanceParams }) {
   const { displayName } = await api.institution.instance.get({ params });
 
-  return <Heading>{displayName}</Heading>;
+  const roles = await api.user.roles({ params });
+  const hasMultipleRoles = roles.size > 1;
+
+  return (
+    <PanelWrapper>
+      <Heading>{displayName}</Heading>
+      {roles.has(Role.ADMIN) && (
+        <>
+          <AdminHome params={params} hasMultipleRoles={hasMultipleRoles} />
+          {(roles.has(Role.SUPERVISOR) || roles.has(Role.READER)) && (
+            <Separator className="my-10" />
+          )}
+        </>
+      )}
+      {roles.has(Role.SUPERVISOR) && (
+        <>
+          <SupervisorHome params={params} hasMultipleRoles={hasMultipleRoles} />
+          {roles.has(Role.READER) && <Separator className="my-10" />}
+        </>
+      )}
+      {roles.has(Role.READER) && (
+        <ReaderHome params={params} hasMultipleRoles={hasMultipleRoles} />
+      )}
+      {roles.has(Role.STUDENT) && <StudentHome params={params} />}
+      <JoinInstance />
+    </PanelWrapper>
+  );
 }
