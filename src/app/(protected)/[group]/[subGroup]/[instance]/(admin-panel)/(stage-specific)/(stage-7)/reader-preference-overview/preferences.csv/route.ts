1dfc775ea4c4cef88647f3bdb69acfe4645403cd
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2025-10-01 14:52:39 +0100

    added preference download CSV Page

---
+++ src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-specific)/(stage-7)/reader-preference-overview/preferences.csv/route.ts
@@ -0,0 +1,61 @@
+import { env } from "@/env";
+import { NextResponse } from "next/server";
+import { unparse } from "papaparse";
+
+import { api } from "@/lib/trpc/server";
+import { type InstanceParams } from "@/lib/validations/params";
+
+// I know this is a radical departure from how we normally handle CSV downloads
+// But I think it's a cleaner way to handle them
+
+interface CSVRow {
+  readerId: string;
+  projectId: string;
+  type: "CONFLICT" | "PREFERABLE" | "UNACCEPTABLE";
+}
+
+export async function GET(
+  _request: Request,
+  { params }: { params: InstanceParams },
+) {
+  // We have to do our own AC here (since there is no layout to do it for us):
+  const isAdmin = await api.ac.adminInInstance({ params });
+  // TODO use real link
+  if (!isAdmin) return Response.redirect(`${env.FRONTEND_SERVER_URL}/`);
+
+  // We then get the data we need:
+  const readerPreferences =
+    await api.institution.instance.getAllReaderPreferences({ params });
+
+  const data: CSVRow[] = readerPreferences
+    .flatMap((r) => {
+      const readerId = r.id;
+      const conflicts = r.conflict.map((e) => ({
+        readerId,
+        projectId: e,
+        type: "CONFLICT" as const,
+      }));
+
+      const prefs = r.preferable.map((e) => ({
+        readerId,
+        projectId: e,
+        type: "PREFERABLE" as const,
+      }));
+
+      const unacceptable = r.unacceptable.map((e) => ({
+        readerId,
+        projectId: e,
+        type: "UNACCEPTABLE" as const,
+      }));
+
+      return [conflicts, prefs, unacceptable];
+    })
+    .flat();
+
+  // unparse it...
+  const csvText = unparse(data);
+
+  // And then we can just return it as a plain response;
+  // The result is we get a preference CSV
+  return new NextResponse(csvText);
+}
