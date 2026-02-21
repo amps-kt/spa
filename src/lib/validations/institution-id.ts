fbc55297c4a513d5b0d6b35de628f18cb1b42da1
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2026-02-21 16:09:50 +0000

    cleaning up some more utils

--- src/lib/validations/institution-id.ts
+++ src/lib/validations/institution-id.ts
@@ -2,7 +2,9 @@ import { z } from "zod";
 
 import { INSTITUTION } from "@/config/institution";
 
-import { isAlphanumeric } from "./is-alphanumeric";
+export const isAlphanumeric = (
+  errMsg = "Only alphanumeric characters are allowed",
+) => z.string().regex(/^[a-zA-Z0-9]+$/, errMsg);
 
 export const institutionIdSchema = z.coerce
   .string<string>(`Please enter a valid ${INSTITUTION.ID_NAME}`)
