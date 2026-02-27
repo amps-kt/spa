fccfdb4d2b2d9fe03ee25b2d8ef5fb5d9215ab70
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2026-02-27 12:54:30 +0000

    added new tests for auto-resolve

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,7 @@
+import tsconfigPaths from "vite-tsconfig-paths";
 import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: { exclude: [...configDefaults.exclude, ".react-email/*"] },
 });
