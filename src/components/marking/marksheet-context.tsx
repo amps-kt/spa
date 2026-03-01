7bb09260c64d37337d2e93f561d84372a8182246
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2026-03-01 15:51:04 +0000

    updated context with new 'viewer role' field

--- src/components/marking/marksheet-context.tsx
+++ src/components/marking/marksheet-context.tsx
@@ -6,12 +6,23 @@ import { type ReaderDTO, type SupervisorDTO } from "@/dto";
 
 import { type InstanceParams } from "@/lib/validations/params";
 
+export const MarksheetRole = {
+  ADMIN: "ADMIN",
+  SUPERVISOR: "SUPERVISOR",
+  READER: "READER",
+  SUPERVISOR_ADMIN: "SUPERVISOR_ADMIN",
+  READER_ADMIN: "READER_ADMIN",
+} as const;
+
+export type MarksheetRole = keyof typeof MarksheetRole;
+
 interface MarksheetContextType {
   reader: ReaderDTO;
   supervisor: SupervisorDTO;
   studentId: string;
-  markerId: string;
   params: InstanceParams;
+  viewerRole: MarksheetRole;
+  userId: string;
 }
 
 const marksheetContext = createContext<MarksheetContextType | undefined>(
@@ -22,13 +33,24 @@ export function MarksheetContextProvider({
   reader,
   supervisor,
   studentId,
-  markerId,
   params,
+  userId,
+  isAdmin,
   children,
-}: { children: ReactNode } & MarksheetContextType) {
+}: {
+  children: ReactNode;
+  isAdmin: boolean;
+  reader: ReaderDTO;
+  supervisor: SupervisorDTO;
+  studentId: string;
+  params: InstanceParams;
+  userId: string;
+}) {
+  const viewerRole = getViewerRole({ userId, isAdmin, supervisor, reader });
+
   return (
     <marksheetContext.Provider
-      value={{ reader, supervisor, studentId, markerId, params }}
+      value={{ reader, supervisor, studentId, params, viewerRole, userId }}
     >
       {children}
     </marksheetContext.Provider>
@@ -44,4 +66,28 @@ export function useMarksheetContext(): MarksheetContextType {
   }
 
   return data;
+}
+
+function getViewerRole({
+  userId,
+  isAdmin,
+  supervisor,
+  reader,
+}: {
+  userId: string;
+  isAdmin: boolean;
+  supervisor: SupervisorDTO;
+  reader: ReaderDTO;
+}): MarksheetRole {
+  if (userId === supervisor.id) {
+    return isAdmin ? MarksheetRole.SUPERVISOR_ADMIN : MarksheetRole.SUPERVISOR;
+  }
+  if (userId === reader.id) {
+    return isAdmin ? MarksheetRole.READER_ADMIN : MarksheetRole.READER;
+  }
+  if (isAdmin) return MarksheetRole.ADMIN;
+
+  console.log(supervisor, reader);
+
+  throw new Error("User who is not marker or admin can see marksheet page");
 }
