91ce2d8e26bb636f259d854c324b721efbeaf84f
Petros <93052465+pkitazos@users.noreply.github.com>
2026-03-11 17:02:14 +0100

    hotfix: merge fixes

--- src/server/routers/institution/instance/teachingOffice.ts
+++ src/server/routers/institution/instance/teachingOffice.ts
@@ -1,6 +1,12 @@
 import { z } from "zod";
 
 import {
+  studentDtoSchema,
+  unitOfAssessmentDtoSchema,
+  unitGradeDtoSchema,
+  type UnitGradeDTO,
+} from "@/dto";
+import {
   studentDeltaSchema,
   studentSubmissionsRowDtoSchema,
 } from "@/dto/marking/student-submissions";
@@ -8,7 +14,42 @@ import {
 import { procedure } from "@/server/middleware";
 import { createTRPCRouter } from "@/server/trpc";
 
+import { keyBy } from "@/lib/utils/key-by";
+
 export const teachingOfficeRouter = createTRPCRouter({
+  getStudentSubmissionInfo: procedure.instance.subGroupAdmin
+    .output(
+      z.array(
+        z.object({
+          student: studentDtoSchema.extend({ enrolled: z.boolean() }),
+          units: z.array(
+            z.object({
+              unit: unitOfAssessmentDtoSchema,
+              grade: unitGradeDtoSchema,
+            }),
+          ),
+        }),
+      ),
+    )
+    .query(async ({ ctx: { instance } }) => {
+      const students = await instance.getStudents();
+      const flags = await instance.getFlagsWithAssessmentDetails();
+
+      const flagsById = keyBy(flags, (flag) => flag.id);
+
+      return students.map((student) => ({
+        student,
+        units: flagsById[student.flag.id].unitsOfAssessment.map((unit) => ({
+          unit,
+          grade: {
+            submitted: true,
+            customDueDate: unit.studentSubmissionDeadline,
+            customWeight: unit.weight,
+          } as unknown as UnitGradeDTO,
+        })),
+      }));
+    }),
+
   getFlagStudentSubmissionInfo: procedure.instance.subGroupAdmin
     .input(z.object({ flagId: z.string() }))
     .output(
