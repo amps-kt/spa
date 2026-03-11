f72960841615f3e4ea3f9abc5a24f2320c36455e
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2026-03-11 11:58:55 +0000

    commented out broken pages

--- src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-specific)/(stage-9)/marking-overview/page.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-specific)/(stage-9)/marking-overview/page.tsx
@@ -1,29 +1,23 @@
-import { metadataTitle, app } from "@/config/meta";
-import { PAGES } from "@/config/pages";
-
-import { Heading } from "@/components/heading";
+// import { metadataTitle, app } from "@/config/meta";
+// import { PAGES } from "@/config/pages";
+// import { Heading } from "@/components/heading";
 import { PanelWrapper } from "@/components/panel-wrapper";
 
-import { api } from "@/lib/trpc/server";
+// import { api } from "@/lib/trpc/server";
 import { type InstanceParams } from "@/lib/validations/params";
 
-import { MarkingOverviewTable } from "./marking-overview-table";
+// import { MarkingOverviewTable } from "./marking-overview-table";
 
-export async function generateMetadata({ params }: { params: InstanceParams }) {
-  const { displayName } = await api.institution.instance.get({ params });
+// export async function generateMetadata({ params }: { params: InstanceParams }) {
+//   const { displayName } = await api.institution.instance.get({ params });
 
-  return {
-    title: metadataTitle([PAGES.markingOverview.title, displayName, app.name]),
-  };
-}
+//   return {
+//     title: metadataTitle([PAGES.markingOverview.title, displayName, app.name]),
+//   };
+// }
 
 export default async function Page({ params }: { params: InstanceParams }) {
-  const data = await api.marking.byProjectMarkingSummary({ params });
+  // const data = await api.marking.byProjectMarkingSummary({ params });
 
-  return (
-    <PanelWrapper className="gap-10">
-      <Heading>{PAGES.markingOverview.title}</Heading>
-      <MarkingOverviewTable data={data} />
-    </PanelWrapper>
-  );
+  return <PanelWrapper className="gap-10">WIP</PanelWrapper>;
 }
--- src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-specific)/(stage-9)/marking-overview/prep-csv.ts
+++ src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-specific)/(stage-9)/marking-overview/prep-csv.ts
@@ -1,151 +1,151 @@
-import { Grade } from "@/logic/grading";
+// import { Grade } from "@/logic/grading";
 
-import { MarkerType } from "@/db/types";
+// import { MarkerType } from "@/db/types";
 
-import {
-  type MarkerStatusSummary,
-  type ProjectMarkingOverview,
-  type UnitGradingStatus,
-  type UnitMarkingSummary,
-} from "./row";
+// import {
+//   type MarkerStatusSummary,
+//   type ProjectMarkingOverview,
+//   type UnitGradingStatus,
+//   type UnitMarkingSummary,
+// } from "./row";
 
-// TODO: this file needs jesus a little bit
+// // TODO: this file needs jesus a little bit
 
-interface CSVRow {
-  studentGUID: string;
-  studentName: string;
-  studentFlag: string;
-  studentEmail: string;
-  projectTitle: string;
+// interface CSVRow {
+//   studentGUID: string;
+//   studentName: string;
+//   studentFlag: string;
+//   studentEmail: string;
+//   projectTitle: string;
 
-  supervisorName: string;
-  supervisorEmail: string;
-  readerName: string;
-  readerEmail: string;
+//   supervisorName: string;
+//   supervisorEmail: string;
+//   readerName: string;
+//   readerEmail: string;
 
-  moderatorName?: string;
-  moderatorEmail?: string;
+//   moderatorName?: string;
+//   moderatorEmail?: string;
 
-  presentationGrade: string;
-  presentationComments: string;
-  conductGrade: string;
-  conductComments: string;
+//   presentationGrade: string;
+//   presentationComments: string;
+//   conductGrade: string;
+//   conductComments: string;
 
-  supervisorDissertationGrade: string;
-  supervisorDissertationComments: string;
-  readerDissertationGrade: string;
-  readerDissertationComments: string;
+//   supervisorDissertationGrade: string;
+//   supervisorDissertationComments: string;
+//   readerDissertationGrade: string;
+//   readerDissertationComments: string;
 
-  requiredNegotiation: boolean;
-  negotiatedGrade?: string;
-  negotiatedComment?: string;
+//   requiredNegotiation: boolean;
+//   negotiatedGrade?: string;
+//   negotiatedComment?: string;
 
-  requiredModeration: boolean;
-  moderatedGrade?: string;
-  moderationComments?: string;
+//   requiredModeration: boolean;
+//   moderatedGrade?: string;
+//   moderationComments?: string;
 
-  finalDissertationGrade: string;
-  overallGrade: string;
-  penalty?: string;
-}
+//   finalDissertationGrade: string;
+//   overallGrade: string;
+//   penalty?: string;
+// }
 
-export function prepCSV(data: ProjectMarkingOverview[]): CSVRow[] {
-  return data.map(({ project, status, student, units }) => {
-    const unitMap = units.reduce(
-      (acc, val) => ({ ...acc, [val.unit.title]: val }),
-      {} as Record<string, UnitMarkingSummary>,
-    );
+// export function prepCSV(data: ProjectMarkingOverview[]): CSVRow[] {
+//   return data.map(({ project, status, student, units }) => {
+//     const unitMap = units.reduce(
+//       (acc, val) => ({ ...acc, [val.unit.title]: val }),
+//       {} as Record<string, UnitMarkingSummary>,
+//     );
 
-    const dissertation = unitMap.Dissertation;
-    const presentation = unitMap.Presentation;
-    const conduct = unitMap.Conduct;
+//     const dissertation = unitMap.Dissertation;
+//     const presentation = unitMap.Presentation;
+//     const conduct = unitMap.Conduct;
 
-    const markerMap = dissertation.markers.reduce(
-      (acc, val) => ({ ...acc, [val.markerType]: val }),
-      {} as Record<MarkerType, MarkerStatusSummary>,
-    );
+//     const markerMap = dissertation.markers.reduce(
+//       (acc, val) => ({ ...acc, [val.markerType]: val }),
+//       {} as Record<MarkerType, MarkerStatusSummary>,
+//     );
 
-    const supervisor = markerMap[MarkerType.SUPERVISOR];
-    const reader = markerMap[MarkerType.READER];
-    const supervisorDissertationGrade = statusToString(supervisor.status);
-    const readerDissertationGrade = statusToString(reader.status);
+//     const supervisor = markerMap[MarkerType.SUPERVISOR];
+//     const reader = markerMap[MarkerType.READER];
+//     const supervisorDissertationGrade = statusToString(supervisor.status);
+//     const readerDissertationGrade = statusToString(reader.status);
 
-    const supervisorDissertationComments =
-      supervisor.status.status === "MARKED" ? supervisor.status.comment : "";
-    const readerDissertationComments =
-      reader.status.status === "MARKED" ? reader.status.comment : "";
+//     const supervisorDissertationComments =
+//       supervisor.status.status === "MARKED" ? supervisor.status.comment : "";
+//     const readerDissertationComments =
+//       reader.status.status === "MARKED" ? reader.status.comment : "";
 
-    const dissStatus = Grade.autoResolve(
-      supervisorDissertationGrade,
-      readerDissertationGrade,
-    );
+//     const dissStatus = Grade.autoResolve(
+//       supervisorDissertationGrade,
+//       readerDissertationGrade,
+//     );
 
-    const requiredNegotiation =
-      dissStatus.status === "NEGOTIATE1" || dissStatus.status === "NEGOTIATE2";
+//     const requiredNegotiation =
+//       dissStatus.status === "NEGOTIATE1" || dissStatus.status === "NEGOTIATE2";
 
-    const dissGrade =
-      dissertation.status.status === "MARKED"
-        ? dissertation.status.grade
-        : undefined;
+//     const dissGrade =
+//       dissertation.status.status === "MARKED"
+//         ? dissertation.status.grade
+//         : undefined;
 
-    const requiredModeration =
-      dissStatus.status === "MODERATE" ||
-      ((!!dissGrade && Grade.checkExtremes(dissGrade).status === "MODERATE") ??
-        false);
+//     const requiredModeration =
+//       dissStatus.status === "MODERATE" ||
+//       ((!!dissGrade && Grade.checkExtremes(dissGrade).status === "MODERATE") ??
+//         false);
 
-    return {
-      studentGUID: student.id,
-      studentName: student.name,
-      studentFlag: student.flag.displayName,
-      studentEmail: student.email,
-      projectTitle: project.title,
+//     return {
+//       studentGUID: student.id,
+//       studentName: student.name,
+//       studentFlag: student.flag.displayName,
+//       studentEmail: student.email,
+//       projectTitle: project.title,
 
-      supervisorName: supervisor.marker.name,
-      supervisorEmail: supervisor.marker.email,
-      readerName: reader.marker.name,
-      readerEmail: reader.marker.email,
+//       supervisorName: supervisor.marker.name,
+//       supervisorEmail: supervisor.marker.email,
+//       readerName: reader.marker.name,
+//       readerEmail: reader.marker.email,
 
-      moderatorName: requiredModeration ? "Paul Harvey" : undefined,
-      moderatorEmail: requiredModeration
-        ? "Paul.Harvey@glasgow.ac.uk"
-        : undefined,
-      moderationComments: undefined,
+//       moderatorName: requiredModeration ? "Paul Harvey" : undefined,
+//       moderatorEmail: requiredModeration
+//         ? "Paul.Harvey@glasgow.ac.uk"
+//         : undefined,
+//       moderationComments: undefined,
 
-      presentationGrade: statusToString(presentation.status) ?? "",
-      conductGrade: statusToString(conduct.status) ?? "",
-      supervisorDissertationGrade: supervisorDissertationGrade ?? "",
-      readerDissertationGrade: readerDissertationGrade ?? "",
+//       presentationGrade: statusToString(presentation.status) ?? "",
+//       conductGrade: statusToString(conduct.status) ?? "",
+//       supervisorDissertationGrade: supervisorDissertationGrade ?? "",
+//       readerDissertationGrade: readerDissertationGrade ?? "",
 
-      presentationComments:
-        presentation.markers[0].status.status === "MARKED"
-          ? presentation.markers[0].status.comment
-          : "",
-      conductComments:
-        conduct.markers[0].status.status === "MARKED"
-          ? conduct.markers[0].status.comment
-          : "",
-      supervisorDissertationComments: supervisorDissertationComments,
-      readerDissertationComments: readerDissertationComments,
+//       presentationComments:
+//         presentation.markers[0].status.status === "MARKED"
+//           ? presentation.markers[0].status.comment
+//           : "",
+//       conductComments:
+//         conduct.markers[0].status.status === "MARKED"
+//           ? conduct.markers[0].status.comment
+//           : "",
+//       supervisorDissertationComments: supervisorDissertationComments,
+//       readerDissertationComments: readerDissertationComments,
 
-      requiredNegotiation,
-      negotiatedGrade: requiredNegotiation
-        ? statusToString(dissertation.status)
-        : undefined,
-      negotiatedComment:
-        requiredNegotiation && dissertation.status.status == "MARKED"
-          ? dissertation.status.comment
-          : undefined,
+//       requiredNegotiation,
+//       negotiatedGrade: requiredNegotiation
+//         ? statusToString(dissertation.status)
+//         : undefined,
+//       negotiatedComment:
+//         requiredNegotiation && dissertation.status.status == "MARKED"
+//           ? dissertation.status.comment
+//           : undefined,
 
-      requiredModeration,
-      moderatedGrade: undefined,
-      finalDissertationGrade: statusToString(dissertation.status) ?? "",
-      overallGrade: statusToString(status) ?? "",
-      penalty: undefined,
-    };
-  });
-}
+//       requiredModeration,
+//       moderatedGrade: undefined,
+//       finalDissertationGrade: statusToString(dissertation.status) ?? "",
+//       overallGrade: statusToString(status) ?? "",
+//       penalty: undefined,
+//     };
+//   });
+// }
 
-function statusToString(data: UnitGradingStatus): string | undefined {
-  if (data.status === "MARKED") return Grade.toLetter(data.grade);
-  return undefined;
-}
+// function statusToString(data: UnitGradingStatus): string | undefined {
+//   if (data.status === "MARKED") return Grade.toLetter(data.grade);
+//   return undefined;
+// }
