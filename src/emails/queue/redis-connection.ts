ab9d8e5df49c00e783c719bc5c0d52383f545cea
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2026-04-03 18:16:37 +0100

    added option to switch on/off rate limit

--- src/emails/queue/redis-connection.ts
+++ src/emails/queue/redis-connection.ts
@@ -1,4 +1,7 @@
 import { env } from "@/env";
 import IORedis from "ioredis";
 
-export const connection = new IORedis({ port: env.REDIS_PORT });
+export const connection = new IORedis({
+  port: env.REDIS_PORT,
+  maxRetriesPerRequest: null,
+});
