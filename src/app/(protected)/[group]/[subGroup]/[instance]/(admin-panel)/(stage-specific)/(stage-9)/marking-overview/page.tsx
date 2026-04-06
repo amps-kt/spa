e3c6cf02056dedcee998f3173516d5412f971ae5
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2026-04-06 12:22:04 +0100

    completed first table for marking overview page

--- src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-specific)/(stage-9)/marking-overview/page.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-specific)/(stage-9)/marking-overview/page.tsx
@@ -1,23 +1,41 @@
-// import { metadataTitle, app } from "@/config/meta";
-// import { PAGES } from "@/config/pages";
-// import { Heading } from "@/components/heading";
-import { PanelWrapper } from "@/components/panel-wrapper";
+import { ZapIcon } from "lucide-react";
 
-// import { api } from "@/lib/trpc/server";
+import { metadataTitle, app } from "@/config/meta";
+import { PAGES } from "@/config/pages";
+
+import { Heading, SectionHeading } from "@/components/heading";
+import { PanelWrapper } from "@/components/panel-wrapper";
+import { Card, CardContent } from "@/components/ui/card";
+
+import { api } from "@/lib/trpc/server";
 import { type InstanceParams } from "@/lib/validations/params";
 
-// import { MarkingOverviewTable } from "./marking-overview-table";
+import { MarkingOverviewTabs } from "./tabs";
 
-// export async function generateMetadata({ params }: { params: InstanceParams }) {
-//   const { displayName } = await api.institution.instance.get({ params });
+export async function generateMetadata({ params }: { params: InstanceParams }) {
+  const { displayName } = await api.institution.instance.get({ params });
 
-//   return {
-//     title: metadataTitle([PAGES.markingOverview.title, displayName, app.name]),
-//   };
-// }
+  return {
+    title: metadataTitle([PAGES.markingOverview.title, displayName, app.name]),
+  };
+}
 
 export default async function Page({ params }: { params: InstanceParams }) {
-  // const data = await api.marking.byProjectMarkingSummary({ params });
+  return (
+    <PanelWrapper className="gap-10">
+      <Heading>{PAGES.markingOverview.title}</Heading>
 
-  return <PanelWrapper className="gap-10">WIP</PanelWrapper>;
+      <section className="flex flex-col gap-5">
+        <SectionHeading icon={ZapIcon}>Quick Actions</SectionHeading>
+        <Card className="w-full">
+          <CardContent className="mt-6 flex items-center justify-between gap-10">
+            <p>Notify all late markers</p>
+            <p>Download CSVs</p>
+          </CardContent>
+        </Card>
+      </section>
+
+      <MarkingOverviewTabs params={params} />
+    </PanelWrapper>
+  );
 }
---
+++ src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-specific)/(stage-9)/marking-overview/tabs.tsx
@@ -0,0 +1,37 @@
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+
+import { api } from "@/lib/trpc/server";
+import { InstanceParams } from "@/lib/validations/params";
+
+import { ByStudentsTable } from "./by-student-table";
+
+export async function MarkingOverviewTabs({
+  params,
+}: {
+  params: InstanceParams;
+}) {
+  const studentMarkingBreakdown =
+    await api.msp.admin.instance.getStudentMarkingStatus({ params });
+
+  return (
+    <Tabs
+      options={["students", "markers"]}
+      defaultValue="students"
+      searchParamName="tab"
+    >
+      <TabsList>
+        <TabsTrigger value="students">By Student</TabsTrigger>
+        <TabsTrigger value="markers">By Marker</TabsTrigger>
+      </TabsList>
+      <TabsContent value="students">
+        <ByStudentsTable
+          params={params}
+          initialData={studentMarkingBreakdown}
+        />
+      </TabsContent>
+      <TabsContent value="markers">
+        <p>marker table</p>
+      </TabsContent>
+    </Tabs>
+  );
+}
