4872c752db5934f2f2733700a7a144595a8da511
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2026-04-06 12:16:35 +0100

    updated student grading lifecycle badge

--- src/components/ui/badges/student-grading-lifecycle-badge.tsx
+++ src/components/ui/badges/student-grading-lifecycle-badge.tsx
@@ -2,6 +2,8 @@ import { cva } from "class-variance-authority";
 
 import { StudentGradingLifecycleState } from "@/dto/marking";
 
+import { cn } from "@/lib/utils";
+
 import { Badge } from "../badge";
 import { WithTooltip } from "../tooltip-wrapper";
 
@@ -22,23 +24,36 @@ const markingStatusData: Record<
   StudentGradingLifecycleState,
   { label: string; tip: string }
 > = {
-  CLOSED: { label: "Closed", tip: "" },
-  DONE: { label: "Done", tip: "" },
-  NOT_SUBMITTED: { label: "Not Submitted", tip: "" },
-  PENDING: { label: "Pending", tip: "" },
-  ACTION_REQUIRED: { label: "Action Required", tip: "" },
+  CLOSED: { label: "Closed", tip: "Marking for this student is closed" },
+  DONE: { label: "Done", tip: "Marking for this student is complete" },
+  NOT_SUBMITTED: {
+    label: "Not Submitted",
+    tip: "Marking for this student cannot continue until the student submits ",
+  },
+  PENDING: { label: "Pending", tip: "Marking for this student is pending" },
+  ACTION_REQUIRED: {
+    label: "Action Required",
+    tip: "Awaiting marks for this student ",
+  },
 };
 
 export function StudentGradingLifecycleBadge({
+  className,
   status,
 }: {
+  className?: string;
   status: StudentGradingLifecycleState;
 }) {
   const { label, tip } = markingStatusData[status];
   return (
     <WithTooltip tip={tip}>
       <div>
-        <Badge className={statusIndicatorVariants({ variant: status })}>
+        <Badge
+          className={cn(
+            statusIndicatorVariants({ variant: status }),
+            className,
+          )}
+        >
           {label}
         </Badge>
       </div>
