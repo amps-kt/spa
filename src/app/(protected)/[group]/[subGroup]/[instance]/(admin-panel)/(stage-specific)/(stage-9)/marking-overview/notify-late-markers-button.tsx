c5b2b04587ea3f1f1ed0139c6ae2ec90462bcd91
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2026-05-07 16:59:13 +0100

    added blame readout to email popup

--- src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-specific)/(stage-9)/marking-overview/notify-late-markers-button.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-specific)/(stage-9)/marking-overview/notify-late-markers-button.tsx
@@ -3,9 +3,10 @@
 import { SendIcon } from "lucide-react";
 import { toast } from "sonner";
 
-import { type UserDTO } from "@/dto";
+import { type LateBlame, type UserDTO } from "@/dto";
 
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { YesNoAction } from "@/components/yes-no-action";
 
 import { api } from "@/lib/trpc/client";
@@ -16,7 +17,7 @@ export function NotifyLateMarkersButton({
   lateMarkers,
 }: {
   params: InstanceParams;
-  lateMarkers: UserDTO[];
+  lateMarkers: { marker: UserDTO; blame: LateBlame[] }[];
 }) {
   const { mutateAsync: api_notifyLateMarkers } =
     api.msp.admin.instance.notifyLateMarkers.useMutation();
@@ -25,7 +26,7 @@ export function NotifyLateMarkersButton({
     <YesNoAction
       title="Notify late markers"
       description={
-        <>
+        <div>
           <p>
             This will send a single email to any marker who has any overdue unit
             of assessment as of now.
@@ -33,7 +34,33 @@ export function NotifyLateMarkersButton({
             This will email {lateMarkers.length} markers. Do you wish to
             continue?
           </p>
-        </>
+
+          <p className="my-2 text-primary">
+            A breakdown of the markers you are about to email, and why, is
+            below:
+          </p>
+
+          <ScrollArea className="h-[40dvh] text-accent-foreground pr-2 bg-accent rounded-md my-2">
+            <ol>
+              {lateMarkers.map((m) => (
+                <div key={m.marker.id} className="m-2">
+                  <div className="text-lg">{m.marker.name}</div>
+                  <ol className="list-disc">
+                    {m.blame.map((b, i) => (
+                      <li
+                        key={i}
+                        className="flex flex-row justify-between ml-2"
+                      >
+                        <p>{b.student.name}</p>
+                        <p>{b.unit.title}</p>
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+              ))}
+            </ol>
+          </ScrollArea>
+        </div>
       }
       trigger={
         <Button variant="outline">
