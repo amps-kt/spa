b87ce6f494ac3d7babfc4a802d7502ce76dd3880
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2026-09-03 10:36:37 +0100

    used nullish coalescing

--- src/emails/queue/redis-connection.ts
+++ src/emails/queue/redis-connection.ts
@@ -4,13 +4,11 @@ import IORedis from "ioredis";
 let redis: IORedis | undefined;
 
 export const getConnection = () => {
-  if (!redis) {
-    redis = new IORedis({
-      host: env.REDIS_HOST,
-      port: env.REDIS_PORT,
-      maxRetriesPerRequest: null,
-    });
-  }
+  redis ??= new IORedis({
+    host: env.REDIS_HOST,
+    port: env.REDIS_PORT,
+    maxRetriesPerRequest: null,
+  });
 
   return redis;
 };
